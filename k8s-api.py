4a9b2f3ec029ca78724acdf8b69231253584dc39
Vinay Talla <98005486+VinayTalla20@users.noreply.github.com>
2022-10-04 16:15:43 +0530

    Create k8s-api.py

---
+++ k8s-api.py
@@ -0,0 +1,23 @@
+import os
+import yaml
+
+
+conf = input("provide the path of cluster config file: ")
+print("Checking for path:",conf,"\n")
+user = os.getenv('USER')
+print("current user:",user, "\n")
+
+if (os.access(conf, os.R_OK)):
+       print("The path",conf,"has access to the current user",user,"\n")
+
+else:
+       print("user",user,"does not has access instead go with user root or use sudo previlage \n")
+
+
+if(os.path.exists(conf)):
+    with open(conf) as f:
+         results = yaml.load(f, Loader=yaml.FullLoader)
+         try:
+              print ("API-SERVER-URL=",results['clusters'][0]['cluster']['server'],"\n")
+         except:
+             print: "Does'nt exists"
